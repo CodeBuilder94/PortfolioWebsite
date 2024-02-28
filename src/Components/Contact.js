292167f9383c78104b2a92d8cb528ae334d2b5ab
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2024-02-28 12:54:45 -0600

    Got the function that gets message information to work. Started on using Resend to be able to send emails. However there is a POST error.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,19 @@
 import React, {useState} from "react";
-
+import sendEmail from "../email/sendEmail"
 const Contact =()=>
 {
     const [messageName, setMessageName] = useState("");
     const [messageEmail, setMessageEmail]=useState("");
     const [messagebody, setMessageBody]= useState("");
 
-    const send=(formData)=>
+    const send= async(ev)=>
     {
-        /*const name = formData.get("name");
-        const email = formData.get("email");
-        const message = formData.get("message");
-        console.log(`${name} using email ${email} sent ${message}`);*/
-        formData.preventDefault();
-        console.log(messageName);
+        ev.preventDefault();
+        console.log(`${messageName} with email ${messageEmail} sent the following message ${messagebody}`);
+        setMessageBody("");
+        setMessageEmail("");
+        setMessageName("");
+        await sendEmail(messageName, messageEmail, messagebody);
     }
 
     return(
@@ -22,11 +22,11 @@ const Contact =()=>
         <p>Got any questions? Ask away!</p>
         <form id="emailForm" onSubmit={send}>
             <label>Name:</label>
-            <input name="name" value={()=>{setMessageName()}}/>
+            <input name="name" value={messageName} onChange={ev =>{setMessageName(ev.target.value)}}/>
             <label>Email:</label>
-            <input name="email" type="email" value={()=>{setMessageEmail()}}/>
+            <input name="email" type="email" value={messageEmail} onChange={ev=>{setMessageEmail(ev.target.value)}}/>
             <label>Message:</label>
-            <textarea placeholder="Type your message here." name="message" value={()=>{setMessageBody()}}/>
+            <textarea placeholder="Type your message here." name="message" value={messagebody} onChange={ev=>(setMessageBody(ev.target.value))}/>
             <button type="submit">Send</button>
         </form>
         </>
---
+++ src/email/sendEmail.js
@@ -0,0 +1,23 @@
+import { Resend } from "resend";
+
+const resend = new Resend("re_5rVKReVv_PHLSN8ZLox8YFMvWcNqzD1cN");//(process.env.RESEND_API_KEY);
+
+
+const sendEmail = async(name, leadEmail, message)=>
+{
+    const {data, error} = await resend.emails.send({
+        from:'email@tyler.webdev.dev',
+        to:[leadEmail],
+        subject:'Webdevelopment Lead',
+        html:`<h6>Name: ${name}</h6>
+        <p><strong>Message</strong>:${message}</p>`
+
+    });
+    if(error)
+    {
+        return console.error({error});
+    }
+    console.log(data);
+};
+
+export default sendEmail;
