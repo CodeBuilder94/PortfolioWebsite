a9c5f381227dbcfeebedbeec503f5fe058803735
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2024-03-15 12:30:48 -0500

    Working on getting a time line working properly.

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,29 +4,33 @@ const Projects =() =>
 {
     return(
         <>
+        </>
+        /*<>
+        <div id="timeLine">
+            <div className="left">
+            <a href="https://pusheen-bazaar.onrender.com/"><img className="webPhoto" src ="/src/images/PusheenBazaarPhoto.png" alt="Pusheen Bazaar"/></a>
+            </div>
+            
+            <div className="right">
+                <div className="Blurb">
+                    <h3 className="siteName">Pusheen Bazaar</h3>
+                    <p className="description">Pusheen Bazaar was a colaberitive project to make a e-commerse web site. The topic and theme of said 
+                        site was up to us. Our group camp up with a site that sold magical items. As for the nut and bolts of the project we had to create a database and a API to interact with it.
+                        We also had to design the front end of the website so that users can, register, login, put items in a cart, and "purchase" said items.
+                        Props to my team for all the hard work they did.</p>
+                </div>
+            </div>
+        </div>
+        </>*/
+        /*<>
             <h2>My Projects</h2>
             <p>Take a look at the varius project that I have worked on by my self or with others.</p>
             <div id="timeLine">
                 <div className="chunck">
-                    <a href="https://pusheen-bazaar.onrender.com/"><img className="webPhoto" src ="/src/images/PusheenBazaarPhoto.png" alt="Pusheen Bazaar"/></a>
-                    <div className="divider">
-                        <div className="round"></div>
-                        <div className="line"></div>
-                    </div>
-                    <div className="Blurb">
-                        <h3 className="siteName">Pusheen Bazaar</h3>
-                        <p className="description">Pusheen Bazaar was a colaberitive project to make a e-commerse web site. The topic and theme of said 
-                        site was up to us. Our group camp up with a site that sold magical items. As for the nut and bolts of the project we had to create a database and a API to interact with it.
-                        We also had to design the front end of the website so that users can, register, login, put items in a cart, and "purchase" said items.
-                        Props to my team for all the hard work they did.</p>
-                        </div>
-                </div>
+
+                    
                 <div className="chunck">
                     <a href="https://merry-begonia-1fbad0.netlify.app"><img className="webPhoto" src="/src/images/StrangersThings.png" alt="Stranges Things"/></a>
-                    <div className="divider">
-                        <div className="round"></div>
-                        <div className="line"></div>
-                    </div>
                     <div className="Blurb">
                         <h3 className="siteName">Strangers Things</h3>
                         <p className="description">Strangers Things is a website built to practise handaling API integration with the front-end of a web site.
@@ -35,7 +39,7 @@ const Projects =() =>
                     </div>
                 </div>
             </div>
-        </>
+        </>*/
     )
 }
 
