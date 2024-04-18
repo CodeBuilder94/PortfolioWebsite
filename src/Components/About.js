d2393a1bc8d0f724ba16de774a1a6a68c17d8aa2
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2024-04-18 12:54:08 -0500

    Started working on the About page.

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,12 @@ const About =() =>
 {
     return(
         <>
-        <h3>About Tyler</h3>
+        <h3>Who is Tyler?</h3>
+        <img alt="Photo of Tyler"></img>
+        <p>Hey, I'm Tyler. Thanks for stopping by my little corner of the internet.</p>
+        <p>My goal is to provide an afordable web development servise to as many people as possible. Sometimes 
+            you need something more flexible and robust than what a Wordpress can give you. That is what I aim to deliver.
+        </p>
         </>
     )
 }
