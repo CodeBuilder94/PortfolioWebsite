3e7422f673ad0d3881dcfa93e3a12c9397a2554d
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2024-04-24 13:00:50 -0500

    Added more images and started on the pricing page details.

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -4,7 +4,15 @@ const Pricing = () =>
 {
     return(
         <>
-        <h2>Pricing</h2>
+            <h2>Pricing</h2>
+            <p>The following are the different services that I offer. Purchasing the service can be done on my <a>Fiverr</a>.</p>
+            <div id="services">
+                <div className="service">
+                    <h2>Front End Only</h2>
+                    <p>Have fully designed and styled front end website.</p>
+                </div>
+            </div>
+
         </>
     )
 }
