56085f87eb3b4f54e0def2ea2ccbc1e8c48d6ac8
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2025-08-19 21:33:10 -0500

    Positioned the text in the pricing section of the site.

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -4,38 +4,39 @@ const Pricing = () =>
 {
     return(
         <>
-            <h2>Pricing</h2>
-            <p>The following are the different services that I offer. Purchasing the service can be done on my <a>Fiverr</a>. When doing so, please provide information as to what the site will be for.
-            All displaied prices are base line and are subject to increase due to the nature of the project.</p>
-            <div id="services">
-                <div className="service">
-                    <h2>Front End Only</h2>
-                    <h2>$90+</h2>
-                    <ul>
-                        <li>Have customised, fully designed, and styled front end website. Cost subject to complexity of the project</li>
-                        <li>Uses HTML, CSS, JavaScript, and REACT</li>
-                    </ul>
-                    
-                </div>
-                <div className="service">
-                    <h2>Back End Only</h2>
-                    <h2>$80+</h2>
-                    <ul>
-                        <li>Have a fully created database and API to access it from. Cost subject to complexity of the project</li>
-                        <li>Uses Express and Postgresql</li>
-                    </ul>
-                </div>
-                <div className="service">
-                    <h2>Front and Back End</h2> 
-                    <h2>$160+</h2>
-                    <ul>
-                        <li>Have a customised, fully designed, and styled front end.</li>
-                        <li> Have a fully created database and API to access it from. Cost subject to complexity of the project</li>
-                        <li>Uses HTML, CSS, JavaScript, REACT, Express, and Postgresql</li>
-                    </ul>
+            <div id="pricing">
+                <h2>Pricing</h2>
+                <p>The following are the different services that I offer. Purchasing the service can be done on my <a>Fiverr</a>. When doing so, please provide information as to what the site will be for.
+                All displaied prices are base line and are subject to increase due to the nature of the project.</p>
+                <div id="services">
+                    <div className="service">
+                        <h2>Front End Only</h2>
+                        <h2>$90+</h2>
+                        <ul>
+                            <li>Have customised, fully designed, and styled front end website. Cost subject to complexity of the project</li>
+                            <li>Uses HTML, CSS, JavaScript, and REACT</li>
+                        </ul>
+                        
+                    </div>
+                    <div className="service">
+                        <h2>Back End Only</h2>
+                        <h2>$80+</h2>
+                        <ul>
+                            <li>Have a fully created database and API to access it from. Cost subject to complexity of the project</li>
+                            <li>Uses Express and Postgresql</li>
+                        </ul>
+                    </div>
+                    <div className="service">
+                        <h2>Front and Back End</h2> 
+                        <h2>$160+</h2>
+                        <ul>
+                            <li>Have a customised, fully designed, and styled front end.</li>
+                            <li> Have a fully created database and API to access it from. Cost subject to complexity of the project</li>
+                            <li>Uses HTML, CSS, JavaScript, REACT, Express, and Postgresql</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-
         </>
     )
 }
