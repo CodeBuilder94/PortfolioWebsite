898476427fe2f2cdbbeef98bf57bf707ec594150
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2025-09-23 21:42:02 -0500

    Updated the price offerings. Broke them down based on the number of pages needed.

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -8,15 +8,19 @@ const Pricing = () =>
                 <h2 id="priceTitle">Pricing</h2>
                 <p>The following are the different services that I offer. Purchasing the service can be done on my <a>Fiverr</a>. When doing so, please provide information as to what the site will be for.
                 All displaied prices are base line and are subject to increase due to the nature of the project.</p>
+                <p>Uses HTML, CSS, JavaScript, and REACT</p>
                 <div id="services">
                     <div className="service">
-                        <h2>Front End Only</h2>
-                        <h2 className="price">$90+</h2>
-                        <ul>
-                            <li>Have customised, fully designed, and styled front end website. Cost subject to complexity of the project</li>
-                            <li>Uses HTML, CSS, JavaScript, and REACT</li>
-                        </ul>
-                        
+                        <h2>1 to 2 Pages</h2>
+                        <h2 className="price">$25</h2>  
+                    </div>
+                    <div className="service">
+                        <h2>3 to 5 Pages</h2>
+                        <h2 className="price">$50</h2>  
+                    </div>
+                    <div className="service">
+                        <h2>6+ Pages</h2>
+                        <h2 className="price">$90</h2>  
                     </div>
                    { /*<div className="service">
                         <h2>Back End Only</h2>
