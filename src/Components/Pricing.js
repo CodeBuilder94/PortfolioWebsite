1c8f4c37289686bd577f00df50d152658e252fba
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2025-10-07 13:11:06 -0500

    Cleaned up the pricing page. Will still need to add more to it.

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -8,19 +8,16 @@ const Pricing = () =>
                 <h2 id="priceTitle">Pricing</h2>
                 <p>The following are the different services that I offer. Purchasing the service can be done on my <a>Fiverr</a>. When doing so, please provide information as to what the site will be for.
                 All displaied prices are base line and are subject to increase due to the nature of the project.</p>
-                <p>Uses HTML, CSS, JavaScript, and REACT</p>
+                <p>(Uses HTML, CSS, JavaScript, and REACT)</p>
                 <div id="services">
                     <div className="service">
-                        <h2>1 to 2 Pages</h2>
-                        <h2 className="price">$25</h2>  
+                        <h2>1 to 2 Pages: <b>$25</b></h2>
                     </div>
                     <div className="service">
-                        <h2>3 to 5 Pages</h2>
-                        <h2 className="price">$50</h2>  
+                        <h2>3 to 5 Pages: <b>$50</b></h2>
                     </div>
                     <div className="service">
-                        <h2>6+ Pages</h2>
-                        <h2 className="price">$90</h2>  
+                        <h2>6+ Pages: <b>$90</b></h2>
                     </div>
                    { /*<div className="service">
                         <h2>Back End Only</h2>
