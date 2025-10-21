33b6b147f4400d295d39f441323e72d64d19ab74
CodeBuilder94 <tyler.c.mitchell@verizon.net>
2025-10-21 12:15:07 -0500

    Added details and progress to pricing page. The processs session goes over what the process a commissioner is to expect.

--- src/Components/Pricing.js
+++ src/Components/Pricing.js
@@ -10,18 +10,23 @@ const Pricing = () =>
                 All displaied prices are base line and are subject to increase due to the nature of the project.</p>
                 <p>(Uses HTML, CSS, JavaScript, and REACT)</p>
                 <div id="services">
+                    <h2>Options:</h2>
                     <div className="service">
                         <h2>Basic:</h2>
                         <h3>1 to 2 Pages: <b>$25</b></h3>
                     </div>
+                    <p>The basics, get one or two well crafted and designed webpages for one website.</p>
+
                     <div className="service">
                         <h2>Standard:</h2>
                         <h3>3 to 5 Pages: <b>$50</b></h3>
                     </div>
+                    <p>The standard, get a website with up to five individual well crafted and designed pages.</p>
                     <div className="service">
                         <h2>Premium:</h2>
-                        <h3>6+ Pages: <b>$90</b></h3>
+                        <h3>6+ Pages: <b>$90+</b></h3>
                     </div>
+                    <p>Gives you the largest website option. Get 6 or more pages. Pricing will scale based on the number of pages and complexity.</p>
                    { /*<div className="service">
                         <h2>Back End Only</h2>
                         <h2 className="price">$80+</h2>
@@ -39,6 +44,15 @@ const Pricing = () =>
                             <li>Uses HTML, CSS, JavaScript, REACT, Express, and Postgresql</li>
                         </ul>
                     </div>*/}
+
+                </div>
+                <div id="details">
+                    <h2>Details:</h2>
+                    <p>Each page is allowed 4 compoents on it. I.e. different sections. Any additional requested compoents are subject to a $10 fee</p>
+                    <h3>Process:</h3>
+                    <p>After I recive your request with details on what you want your page to look like I will provide a mock up. Changes made at this point are free.
+                        After the mock up is locked in I the website will start to be built. I will give updates as the project progresses.</p>
+                        <p>Any major changes/overhauls at this point are subject an additional charge.</p>
                 </div>
             </div>
         </>
